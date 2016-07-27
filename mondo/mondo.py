b8c11eecf9e043d0d4c054e80d85d3fb5577209f
Muyiwa Olu-Ogunleye <m.oluogunleye94@gmail.com>
2016-07-27 22:25:54 +0100

    Updated Mondo to use custom request class.

--- mondo/mondo.py
+++ mondo/mondo.py
@@ -1,6 +1,5 @@
 """A wrapper around the official Mondo API endpoints."""
-import datetime
-import requests
+from mondo.request import Request
 
 class Mondo(object):
     """The class representation of Mondo's API endpoints."""
@@ -8,18 +7,19 @@ class Mondo(object):
     def __init__(self, access_token):
         self.access_token = access_token
         self.headers = {'Authorization': 'Bearer {0}'.format(self.access_token)}
+        self.request = Request()
 
     def whoami(self):
         """Gives information about an access token."""
         url = "{0}/ping/whoami".format(self.API_URL)
-        response = requests.get(url, headers=self.headers)
-        return response.json()
+        response = self.request.get(url, headers=self.headers)
+        return response
 
     def get_accounts(self):
         """Get all accounts that belong to a user."""
         url = "{0}/accounts".format(self.API_URL)
-        response = requests.get(url, headers=self.headers)
-        return response.json()
+        response = self.request.get(url, headers=self.headers)
+        return response
 
     def get_first_account(self):
         """Gets the first account for a user."""
@@ -30,23 +30,24 @@ class Mondo(object):
 
     def get_transactions(self, account_id):
         """Get all transactions of a given account."""
-        url = "{0}/transactions?expand[]=merchant&account_id={1}".format(
-                                                                    self.API_URL,
-                                                                    account_id)
-        response = requests.get(url, headers=self.headers)
-        return response.json()
+        url = "{0}/transactions".format(self.API_URL)
+        params = {'expand[]': 'merchant', 'account_id': account_id}
+        response = self.request.get(url, headers=self.headers, params=params)
+        return response
 
     def get_balance(self, account_id):
         """Gets the balance of a given account."""
-        url = "{0}/balance?account_id={1}".format(self.API_URL, account_id)
-        response = requests.get(url, headers=self.headers)
-        return response.json()
+        url = "{0}/balance".format(self.API_URL)
+        params = {'account_id': account_id}
+        response = self.request.get(url, headers=self.headers, params=params)
+        return response
 
     def get_webhooks(self, account_id):
         """Gets the webhooks of a given account."""
-        url = "{0}/webhooks?account_id={1}".format(self.API_URL, account_id)
-        response = requests.get(url, headers=self.headers)
-        return response.json()
+        url = "{0}/webhooks".format(self.API_URL)
+        params = {'account_id': account_id}
+        response = self.request.get(url, headers=self.headers, params=params)
+        return response
 
     def get_first_webhook(self, account_id):
         """Gets the first webhook of a given account."""
@@ -58,8 +59,8 @@ class Mondo(object):
     def delete_webhook(self, webhook_id):
         """Deletes the a specified webhook."""
         url = "{0}/webhooks/{1}".format(self.API_URL, webhook_id)
-        response = requests.delete(url, headers=self.headers)
-        return response.json()
+        response = self.request.delete(url, headers=self.headers)
+        return response
 
     def delete_all_webhooks(self):
         first_account = self.get_first_account()
@@ -71,7 +72,7 @@ class Mondo(object):
     def register_webhook(self, webhook_url, account_id):
         """Registers a webhook."""
         url = "{0}/webhooks".format(self.API_URL)
-        response = requests.post(url, headers=self.headers, data={'account_id': account_id, 'url': webhook_url})
+        response = self.request.post(url, headers=self.headers, data={'account_id': account_id, 'url': webhook_url})
 
     def attach_image_to_transaction(self, file_url, file_type):
         """Attaches an image to a transaction."""
