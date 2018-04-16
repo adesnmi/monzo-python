375d1677a2ace1e37d8dbea8f23795211bb129d2
Muyiwa Olu <muyiwaolu@users.noreply.github.com>
2018-04-16 20:38:33 +0100

    v0.5.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='monzo',
-      version='0.5.3',
+      version='0.5.4',
       description='A python SDK for interacting with the Monzo API.',
       url='https://github.com/muyiwaolu/monzo-python',
       author='Muyiwa Olu-Ogunleye',
