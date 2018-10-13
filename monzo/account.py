14fa42dcf9ba1bc2a48c4757fbbe8af9ac1fe7fc
Tom French <tom@tomfren.ch>
2018-10-13 01:20:19 +0100

    Removed implementations of __str__()

--- monzo/account.py
+++ monzo/account.py
@@ -8,14 +8,6 @@ class Account(dict):
     def __repr__(self):
         return "{}({!r})".format(self.__class__.__name__, dict(self))
 
-    def __str__(self):
-        string = ""
-        keys = ['id', 'description', 'type', 'account_number', 'sort_code']
-        for key in keys:
-            string.append(key + ': ' + self[key] + '\n')
-        string.append('open: ' + str(self.is_open()))
-        return(string)
-
     def is_open(self):
         return(not self['closed'])
 
@@ -26,15 +18,6 @@ class Pot(dict):
 
     def __repr__(self):
         return "{}({!r})".format(self.__class__.__name__, dict(self))
-
-    def __str__(self):
-        string = ""
-        keys = self.keys
-        keys.remove('deleted')
-        for key in keys:
-            string.append(key + ': ' + self[key] + '\n')
-        string.append('open: ' + str(self.is_open()))
-        return(string)
 
     def is_open(self):
         return(not self['deleted'])
