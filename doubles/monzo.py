30578f29cf22a8cb04e584337f8c51058375bd9c
Dextroz <25749668+Dextroz@users.noreply.github.com>
2019-02-03 18:48:58 +0000

    Updated for get_transaction.

--- doubles/monzo.py
+++ doubles/monzo.py
@@ -298,3 +298,40 @@ class Monzo(object):
                 }
             ]
         }
+    
+    def get_transaction(self, transaction_id):
+        """Retrieve data for a specific transaction. (https://docs.monzo.com/#retrieve-transaction)
+           :param transaction_id: The unique identifier for the transaction for which data should be retrieved for.
+           :rtype: A dictionary containing the data for the specified transaction_id.
+        """
+        return {"transaction": {
+                "account_balance": 13013,
+                "amount": -510,
+                "created": "2015-08-22T12:20:18Z",
+                "currency": "GBP",
+                "description": "THE DE BEAUVOIR DELI C LONDON        GBR",
+                "id": "tx_00008zIcpb1TB4yeIFXMzx",
+                "merchant": {
+                    "address": {
+                        "address": "98 Southgate Road",
+                        "city": "London",
+                        "country": "GB",
+                        "latitude": 51.54151,
+                        "longitude": -0.08482400000002599,
+                        "postcode": "N1 3JD",
+                        "region": "Greater London"
+                    },
+                    "created": "2015-08-22T12:20:18Z",
+                    "group_id": "grp_00008zIcpbBOaAr7TTP3sv",
+                    "id": "merch_00008zIcpbAKe8shBxXUtl",
+                    "logo": "https://pbs.twimg.com/profile_images/527043602623389696/68_SgUWJ.jpeg",
+                    "emoji": "🍞",
+                    "name": "The De Beauvoir Deli Co.",
+                    "category": "eating_out"
+                },
+                "metadata": {},
+                "notes": "Salmon sandwich 🍞",
+                "is_load": false,
+                "settled": "2015-08-23T12:20:18Z"
+            }
+        }
