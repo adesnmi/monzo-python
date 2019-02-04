4b17e03580b93b7607fbb028a6824d2f7aa2fd67
Tom French <tom@tomfren.ch>
2019-02-04 19:43:28 +0000

    Changed conf.py to use relative path to get root directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/Users/muyiwa/Code/monzo-python'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- General configuration ------------------------------------------------
 
